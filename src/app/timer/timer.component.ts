b87c9f56e41f6f2b3a89a81f3f7686b10e9114c5
ArnoutPullen <ArnoutPullen@users.noreply.github.com>
2020-01-23 22:32:54 +0100

    refactor
    tab size indent 2 to 4

--- src/app/timer/timer.component.ts
+++ src/app/timer/timer.component.ts
@@ -26,12 +26,12 @@ export class TimerComponent implements OnInit {
     constructor() { }
 
     ngOnInit() {
-        this.date = new Date("01-23-2020 15:50:58");
+        this.date = new Date('01-23-2020 15:50:58');
         this.startTimer();
         this.audio = new Audio();
-        this.audio.src = "/assets/audio/schoolbel.mp3";
+        this.audio.src = '/assets/audio/schoolbel.mp3';
         this.audio.load();
-        this.date = new Date("01-23-2020 15:50:56");
+        this.date = new Date('01-23-2020 15:50:56');
     }
 
     play() {
@@ -42,10 +42,10 @@ export class TimerComponent implements OnInit {
         const min = this.date.getMinutes();
         const sec = this.date.getSeconds();
 
-        const m = (59 - sec).toString().length == 1 ? '0' + (59 - sec) : 59 - sec;
-        const s = (endMin - sec).toString().length == 1 ? '0' + (endMin - sec) : endMin - sec;
+        const m = (59 - sec).toString().length === 1 ? '0' + (59 - sec) : 59 - sec;
+        const s = (endMin - sec).toString().length === 1 ? '0' + (endMin - sec) : endMin - sec;
 
-        this.countDown = ( 59 - min ) + ':' + s;
+        this.countDown = (59 - min) + ':' + s;
     }
 
     intervaler() {
@@ -68,31 +68,31 @@ export class TimerComponent implements OnInit {
         // check if there is currently a break
         // before 12/14/16/18/20 etc. hours
         if (this.breakType === 'longBreak'
-        && min >= 60 - this.longBreak) {
+            && min >= 60 - this.longBreak) {
             // const m = (59 - sec).toString().length == 1 ? '0' + (59 - sec) : 59 - sec;
             // const s = (60 - sec).toString().length == 1 ? '0' + (60 - sec) : 60 - sec;
             // this.countDown = ( 59 - min ) + ':' + s;
             this.calculateCountDown(60);
             this.status = 'longBreak';
-        // 14:25/14:30 | 14:55/15:00 | 15:25/15:30
+            // 14:25/14:30 | 14:55/15:00 | 15:25/15:30
         } else if (this.breakType === 'shortBreak'
-        && ( hour % 2 === 0 && min >= ( 30 - this.shortBreak ) && min < 30 ) // first: 14:25/14:30
-        || ( hour % 2 === 0 && min >= ( 60 - this.shortBreak ) ) // second: 14:55/15:00
-        || ( hour % 2 === 1 && min >= ( 30 - this.shortBreak ) && min < 30 ) ) { // third: 15:25/15:30
+            && (hour % 2 === 0 && min >= (30 - this.shortBreak) && min < 30) // first: 14:25/14:30
+            || (hour % 2 === 0 && min >= (60 - this.shortBreak)) // second: 14:55/15:00
+            || (hour % 2 === 1 && min >= (30 - this.shortBreak) && min < 30)) { // third: 15:25/15:30
             this.status = 'shortBreak';
         } else {
             this.status = 'iteration';
         }
 
         // Check if break starts
-        if (oldStatus == 'iteration'
-        && ( this.status == 'shortBreak' || this.status == 'longBreak' ) ) {
+        if (oldStatus === 'iteration'
+            && (this.status === 'shortBreak' || this.status === 'longBreak')) {
             this.play();
         }
 
         // Check if break ends
-        if ( ( oldStatus == 'shortBreak' || oldStatus == 'longBreak' )
-        && this.status == 'iteration' ) {
+        if ((oldStatus === 'shortBreak' || oldStatus === 'longBreak')
+            && this.status === 'iteration') {
             this.play();
         }
 
